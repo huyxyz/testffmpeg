661c155c20e23a2f0976b91e6ffbf2e0722959fe
huynq9 <huynq9@gmail.com>
2017-09-06 15:14:06 +0100

    update converting process

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import bottle
+import os
 import re
 import datetime
 from bottle import route, run, template, static_file, request
@@ -15,8 +16,13 @@ def home():
  return template('./static/index.html') 
 
 def makeVideo(key, audio, fr):
- cmd = "ffmpeg -framerate {fr} -i ./upload/{key}%03d.png -i {audio} -c:v libx264 -pix_fmt yuv420p -shortest ./upload/out_{key}.mp4".format(fr = fr , key = key, audio = audio)
+ cmd = "ffmpeg -framerate {fr} -i ./upload/{key}/{key}%03d.png -i {audio} -c:v libx264 -pix_fmt yuv420p -shortest ./upload/{key}/out_{key}.mp4".format(fr = fr , key = key, audio = audio)
  p = subprocess.call(cmd, shell=True)
+ return 1
+
+def convertImg(key):
+ cmd = "ls | grep '.data' | xargs -n 1 bash -c 'convert \"$0\" \"${0%.*}.png\"'"
+ p = subprocess.call(cmd, shell=True, cwd="./upload/{dir}".format(dir = key))
  return 1
 
 @application.route('/upload', method=['POST'])
@@ -29,17 +35,22 @@ def upload():
  key = str(datetime.datetime.now())
  key = re.sub(r"[^\w\s]", '', key)  
  key = re.sub(r"\s+", '', key)
- file1 = "upload/{file}".format(file = key + "001.png")
- file2 = "upload/{file}".format(file = key + "002.png")
- file3 = "upload/{file}".format(file = key + "003.png")
+ dir = "./upload/{d}".format(d = key)
+ if not os.path.exists(dir):
+  os.makedirs(dir)
+ file1 = "{path}/{file}".format(path = dir, file = key + "001.data")
+ file2 = "{path}/{file}".format(path = dir, file = key + "002.data")
+ file3 = "{path}/{file}".format(path = dir, file = key + "003.data")
  upload1.save(file1)
  upload2.save(file2)
  upload3.save(file3)
  print "1. upload finished!"
- print "2. generating video..."
- makeVideo(key, audio, 0.1) 
+ print "2. converting images..."
+ convertImg(key)
+ print "3. generating video..."
+ makeVideo(key, audio, 0.11) 
  print "3. video generated!"
- return "Your video file: </br><a href='/upload/out_" + key +".mp4'>" + key  + ".mp4</a>"
+ return "Your video file: </br><a href='/upload/" + key + "/out_" + key +".mp4'>" + key  + ".mp4</a>"
 
 
 if __name__ == "__main__":
